911eb27f50ae997b0bb289a1721e16acefe15bf7
Roman Frolov <r9888582703@gmail.com>
2026-05-15 14:34:08 +0300

    new changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,13 +17,19 @@ if [ -f "$CONFIG_PATH" ]; then
     DB_NAME=${POSTGRES_DB_SYNAPSE:-synapse}
     DB_HOST=${POSTGRES_HOST:-synapse_db}
 
-    sudo sed -i '/port: 8008/,/type: http/ { /type: http/ a\    x_forwarded: true
-    }' "$CONFIG_PATH"
-
-    sudo sed -i '/database:/,/args:/d' "$CONFIG_PATH"
-    sudo sed -i '/database: sqlite3/d' "$CONFIG_PATH"
+    sudo sed -i '/listeners:/,$d' "$CONFIG_PATH"
 
     sudo tee -a "$CONFIG_PATH" > /dev/null <<EOF
+listeners:
+  - port: 8008
+    tls: false
+    type: http
+    x_forwarded: true
+    resources:
+      - names:
+          - client
+          - federation
+        compress: false
 
 database:
   name: psycopg2
@@ -71,8 +77,18 @@ sleep 60
 chmod +x scripts/create_admin.sh
 ./scripts/create_admin.sh
 
-echo "Применение настроек Nextcloud..."
+echo "Запуск автоматической установки ядра Nextcloud..."
 
+docker compose exec -u www-data nextcloud php occ maintenance:install \
+  --database="pgsql" \
+  --database-name="${POSTGRES_DB_NEXTCLOUD}" \
+  --database-host="nextcloud_db" \
+  --database-user="${POSTGRES_USER}" \
+  --database-pass="${POSTGRES_PASSWORD}" \
+  --admin-user="${ADMIN_USER}" \
+  --admin-pass="${ADMIN_PASSWORD}" --no-interaction || echo "Nextcloud уже установлен."
+
+echo "Применение настроек Nextcloud..."
 docker compose exec -u www-data nextcloud php occ config:system:set trusted_domains 2 --value="${SYNAPSE_SERVER_NAME}" || echo "Предупреждение: не удалось установить trusted_domains"
 docker compose exec -u www-data nextcloud php occ config:system:set trusted_proxies 0 --value='172.16.0.0/12'
 docker compose exec -u www-data nextcloud php occ config:system:set maintenance_window_start --value=2 --type=int
